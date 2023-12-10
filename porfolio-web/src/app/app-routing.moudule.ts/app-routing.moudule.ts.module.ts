4c418b03371df435e282202b09fc2c16ac9f82de
LisanRios <114954842+LisanRios@users.noreply.github.com>
2023-12-10 17:42:11 -0300

    Realización de los ruteos correspondientes

---
+++ porfolio-web/src/app/app-routing.moudule.ts/app-routing.moudule.ts.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '../components/header/header.component';
+import { ProjectsComponent } from '../components/projects/projects.component';
+import { TechnologiesComponent } from '../components/technologies/technologies.component';
+import { ContactComponent } from '../components/contact/contact.component';
+import { Page404Component } from '../components/page404/page404.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'Inicio', pathMatch: 'full'},
+  {path: 'Inicio', component: HeaderComponent},
+  {path: 'Proyectos', component: ProjectsComponent},
+  {path: 'Tecnologias', component: TechnologiesComponent},
+  {path: 'Contacto', component: ContactComponent}, 
+  {path: '**', component: Page404Component},
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)],
+  exports:[RouterModule]
+})
+export class AppRoutingMouduleTsModule { }
