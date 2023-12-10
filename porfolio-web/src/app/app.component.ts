4ac0b75c3dee617e50024d5950f3420d20bab7c8
LisanRios <114954842+LisanRios@users.noreply.github.com>
2023-12-10 15:39:16 -0300

    creación de los componentes

--- porfolio-web/src/app/app.component.ts
+++ porfolio-web/src/app/app.component.ts
@@ -8,3 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'porfolio-web';
 }
+let previousTitle = document.title;
+
+window.addEventListener('blur', () => {
+  previousTitle = document.title;
+  document.title = ' Porfolio web || ¡No te vayas! :(';
+})
+window.addEventListener('focus', () => {
+  document.title = previousTitle;
+})
--- porfolio-web/src/app/app.module.ts
+++ porfolio-web/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { Page404Component } from './components/page404/page404.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { TechnologiesComponent } from './components/technologies/technologies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    Page404Component,
+    ProjectsComponent,
+    TechnologiesComponent
   ],
   imports: [
     BrowserModule,
